38e7e3918f753e5cb329aaf39e91051ff4fa367b
Ryan <lizkaryan626@gmail.com>
2025-01-16 07:34:55 +0300

    bug fix contour

--- obj/Debug/net8.0-windows/IAR.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/IAR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IAR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a1266741ec83e1853c9d44157dace93eda78003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49730d669b422341dea9f47e2be2ff735a860a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("IAR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IAR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
